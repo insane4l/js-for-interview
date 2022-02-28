379b69cff830fd4b14b6d5d8e09d183468fb6bfd
insane4l <43768644+insane4l@users.noreply.github.com>
2022-02-28 15:53:41 +0200

    Browser events

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ console.log(Number.isNaN(NaN)); // true
 
 
 
+
+
+// browser events
+
+el.onclick = funcName // set function
+el.onclick = null // cleanup
+
+// 3 аргумент приходящий в колбек addEventListener - boolean:
+// default false, если передадим true, браузер будет рассматривать событие не как всплывающее
+// а как погружающееся
+// Bubbling - Всплытие
+// Capturing - Погружение
+
+event.target // элемент, который сгенерировал событие (первый получил клик)
+event.currentTarget // элемент который вызвал обработчик в процессе всплытия
+
+event.stopPropagation() // останавливается вызов дальнейших обработчиков(по всплытию)
+event.stopImmediatePropagation() // блокирует вызов других обработчиков, зарегистрированных даже на кликнутом элементе
